cb7d07dd73970ecf4f5b8580f2a9680b562bb437
Rich Romanowski <79664656+rromanowski-figure@users.noreply.github.com>
2025-11-07 10:37:53 -0500

    add error files

---
+++ src/dime/error.rs
@@ -0,0 +1,24 @@
+use quick_error::quick_error;
+
+quick_error! {
+    #[derive(Debug, PartialEq)]
+    pub enum DimeError {
+        BufferSizeError(message: String) { }
+        InvalidMagicBytesError(message: String) { }
+        InvalidVersionError(version: u16) { }
+        InvalidUuidSizeError(size: u32) { }
+        Utf8Error(err: std::string::FromUtf8Error) {
+            from()
+        }
+        ProstDecodeError(err: prost::DecodeError) {
+            from()
+        }
+        // serde_json::Error is converted to a String because it does not implement PartialEq
+        SerdeDecodeError(message: String) { }
+        // serde_json::Error is converted to a String because it does not implement PartialEq
+        SerdeEncodeError(message: String) { }
+        InternalInvalidState(message: String) { }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, DimeError>;
