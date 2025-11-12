9ef923cb447958579a47eb3158a7172326fcd0a2
Rich Romanowski <79664656+rromanowski-figure@users.noreply.github.com>
2025-11-12 16:10:29 -0500

    refactors to prepare for testing

--- src/main.rs
+++ src/main.rs
@@ -11,29 +11,23 @@ mod object;
 mod proto_helpers;
 mod public_key;
 mod replication;
+mod server;
 mod storage;
 mod types;
 
 use crate::cache::Cache;
 use crate::config::Config;
 use crate::mailbox::MailboxGrpc;
-use crate::middleware::{
-    report_datadog_traces, LoggingMiddlewareLayer, MinitraceGrpcMiddlewareLayer, MinitraceSpans,
-};
+use crate::middleware::MinitraceGrpcMiddlewareLayer;
 use crate::object::ObjectGrpc;
 use crate::public_key::PublicKeyGrpc;
 use crate::replication::{reap_unknown_keys, replicate, ReplicationState};
+use crate::server::{base_server, init_health_service, start_trace_reporter};
 use crate::storage::{FileSystem, GoogleCloud, Storage};
 use crate::types::{OsError, Result};
 
-use sqlx::{
-    migrate::Migrator,
-    postgres::{PgPool, PgPoolOptions},
-    Executor,
-};
+use sqlx::{migrate::Migrator, postgres::PgPoolOptions, Executor};
 use std::sync::{Arc, Mutex};
-use tokio::sync::mpsc::channel;
-use tonic::transport::Server;
 
 mod pb {
     tonic::include_proto!("objectstore");
@@ -48,128 +42,94 @@ static MIGRATOR: Migrator = sqlx::migrate!();
 // TODO add logging in Trace middleware
 // TODO implement checksum in filestore
 
-async fn health_status(mut reporter: tonic_health::server::HealthReporter, db: Arc<PgPool>) {
-    log::info!("Starting health status check");
-
-    loop {
-        tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-
-        if let Err(err) = datastore::health_check(&db).await {
-            log::warn!("Failed to health check the database connection {:?}", err);
-
-            reporter
-                .set_service_status("", tonic_health::ServingStatus::NotServing)
-                .await;
-        } else {
-            log::trace!("Database health check success!");
-
-            reporter
-                .set_service_status("", tonic_health::ServingStatus::Serving)
-                .await;
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let mut cache = Cache::default();
-    let config = Config::new();
-    let storage = match config.storage_type.as_str() {
-        "file_system" => {
-            Ok(Box::new(FileSystem::new(config.storage_base_path.as_str())) as Box<dyn Storage>)
-        }
-        "google_cloud" => Ok(Box::new(GoogleCloud::new(
-            config.storage_base_url.clone(),
-            config.storage_base_path.clone(),
-        )) as Box<dyn Storage>),
-        _ => Err(OsError::InvalidApplicationState("".to_owned())),
-    }?;
-    let schema = Arc::new(config.db_schema.clone());
-    let pool = PgPoolOptions::new()
-        .after_connect(move |conn, _meta| {
-            let schema = Arc::clone(&schema);
-            Box::pin(async move {
-                conn.execute(format!("SET search_path = '{}';", &schema).as_ref())
-                    .await?;
+    let config = Arc::new(Config::new());
 
-                Ok(())
+    let storage = {
+        let storage =
+            match config.storage_type.as_str() {
+                "file_system" => Ok(Box::new(FileSystem::new(config.storage_base_path.as_str()))
+                    as Box<dyn Storage>),
+                "google_cloud" => Ok(Box::new(GoogleCloud::new(
+                    config.storage_base_url.clone(),
+                    config.storage_base_path.clone(),
+                )) as Box<dyn Storage>),
+                _ => Err(OsError::InvalidApplicationState("".to_owned())),
+            }?;
+
+        Arc::new(storage)
+    };
+
+    let pool = {
+        let schema = config.db_schema.clone();
+
+        let pool = PgPoolOptions::new()
+            .after_connect(move |conn, _meta| {
+                let schema = schema.clone();
+                Box::pin(async move {
+                    conn.execute(format!("SET search_path = '{}';", schema).as_ref())
+                        .await?;
+
+                    Ok(())
+                })
             })
-        })
-        .max_connections(config.db_connection_pool_size.into())
-        .connect(config.db_connection_string().as_ref())
-        .await?;
+            .max_connections(config.db_connection_pool_size.into())
+            .connect(config.db_connection_string().as_ref())
+            .await?;
 
-    log::debug!("Running migrations");
+        log::debug!("Running migrations");
 
-    MIGRATOR.run(&pool).await?;
+        MIGRATOR.run(&pool).await?;
+
+        Arc::new(pool)
+    };
 
     // populate initial cache
-    for key in datastore::get_all_public_keys(&pool).await? {
-        log::debug!(
-            "Adding public key {} with url {}",
-            &key.public_key,
-            &key.url
-        );
+    let cache = {
+        let mut cache = Cache::default();
+        for key in datastore::get_all_public_keys(&pool).await? {
+            log::debug!(
+                "Adding public key {} with url {}",
+                &key.public_key,
+                &key.url
+            );
 
-        cache.add_public_key(key);
-    }
+            cache.add_public_key(key);
+        }
 
-    let pool = Arc::new(pool);
-    let cache = Arc::new(Mutex::new(cache));
-    let config = Arc::new(config);
-    let storage = Arc::new(storage);
+        Arc::new(Mutex::new(cache))
+    };
 
-    let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
-    let public_key_service = PublicKeyGrpc::new(Arc::clone(&cache), Arc::clone(&pool));
-    let mailbox_service =
-        MailboxGrpc::new(Arc::clone(&cache), Arc::clone(&config), Arc::clone(&pool));
-    let object_service = ObjectGrpc::new(
-        Arc::clone(&cache),
-        Arc::clone(&config),
-        Arc::clone(&pool),
-        Arc::clone(&storage),
-    );
-    let replication_state = ReplicationState::new(
-        Arc::clone(&cache),
-        Arc::clone(&config),
-        Arc::clone(&pool),
-        Arc::clone(&storage),
-    );
-
-    // set initial health status and start a background
-    health_reporter
-        .set_service_status("", tonic_health::ServingStatus::NotServing)
-        .await;
-    tokio::spawn(health_status(health_reporter, Arc::clone(&pool)));
-
-    // start replication
     if config.replication_enabled {
+        let replication_state =
+            ReplicationState::new(cache.clone(), config.clone(), pool.clone(), storage.clone());
+
+        // start replication
         tokio::spawn(replicate(replication_state));
+
         // start unknown reaper - removes replication objects for public_keys that moved from Unknown -> Local
-        tokio::spawn(reap_unknown_keys(Arc::clone(&pool), Arc::clone(&cache)));
+        tokio::spawn(reap_unknown_keys(pool.clone(), cache.clone()));
     }
 
-    // start datadog reporter
-    let (datadog_sender, datadog_receiver) = channel::<MinitraceSpans>(10);
-    if let Some(ref dd_config) = config.dd_config {
-        tokio::spawn(report_datadog_traces(
-            datadog_receiver,
-            dd_config.agent_host,
-            dd_config.agent_port,
-            dd_config.service.clone(),
-        ));
-    }
+    let public_key_service = PublicKeyGrpc::new(cache.clone(), pool.clone());
+    let mailbox_service = MailboxGrpc::new(cache.clone(), config.clone(), pool.clone());
+    let object_service =
+        ObjectGrpc::new(cache.clone(), config.clone(), pool.clone(), storage.clone());
+
+    let health_service = init_health_service(pool.clone()).await;
 
     log::info!("Starting server on {:?}", &config.url);
 
     // TODO add server fields that make sense
     if let Some(ref dd_config) = config.dd_config {
-        Server::builder()
-            .layer(LoggingMiddlewareLayer::new(Arc::clone(&config)))
+        let datadog_sender = start_trace_reporter(dd_config);
+
+        base_server(config.clone())
             .layer(MinitraceGrpcMiddlewareLayer::new(
-                Arc::clone(&config),
+                config.clone(),
                 dd_config.span_tags.clone(),
                 datadog_sender,
             ))
@@ -180,15 +140,14 @@ async fn main() -> Result<()> {
             .serve(config.url)
             .await?;
     } else {
-        Server::builder()
-            .layer(LoggingMiddlewareLayer::new(Arc::clone(&config)))
+        base_server(config.clone())
             .add_service(health_service)
             .add_service(PublicKeyServiceServer::new(public_key_service))
             .add_service(MailboxServiceServer::new(mailbox_service))
             .add_service(ObjectServiceServer::new(object_service))
             .serve(config.url)
             .await?;
-    }
+    };
 
     Ok(())
 }
