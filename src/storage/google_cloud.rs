089d262150d6599cd7b7f59a18fe70b82c14b672
Rich Romanowski <79664656+rromanowski-figure@users.noreply.github.com>
2025-12-17 11:26:28 -0500

    add file path to error

--- src/storage/google_cloud.rs
+++ src/storage/google_cloud.rs
@@ -11,9 +11,12 @@ pub struct GoogleCloud {
 }
 
 impl GoogleCloud {
-
     pub fn new(base_url: Option<String>, bucket: String) -> Self {
-        GoogleCloud { client: Client::default(), base_url, bucket }
+        GoogleCloud {
+            client: Client::default(),
+            base_url,
+            bucket,
+        }
     }
 
     fn base_url_ref(&self) -> Option<&str> {
@@ -35,7 +38,12 @@ impl Storage for GoogleCloud {
 
         self.client
             .object(self.base_url_ref())
-            .create(self.bucket.as_str(), data.to_vec(), &path.file, "application/octet-stream")
+            .create(
+                self.bucket.as_str(),
+                data.to_vec(),
+                &path.file, // TODO add dir? - is this putting all files at top level of bucket?
+                "application/octet-stream",
+            )
             .await
             .map_err(|e| StorageError::IoError(format!("{:?}", e)))?;
 
@@ -44,11 +52,14 @@ impl Storage for GoogleCloud {
 
     #[trace("google_cloud::fetch")]
     async fn fetch(&self, path: &StoragePath, content_length: u64) -> Result<Vec<u8>> {
-        let data = self.client
+        let data = self
+            .client
             .object(self.base_url_ref())
-            .download(self.bucket.as_str(), &path.file)
+            .download(self.bucket.as_str(), &path.file) // TODO surely this needs the dir
             .await
-            .map_err(|e| StorageError::IoError(format!("{:?}", e)))?;
+            .map_err(|e| {
+                StorageError::IoError(format!("Unable to fetch file: {} {:?}", &path.file, e))
+            })?;
 
         if let Err(e) = self.validate_content_length(&path, content_length, &data) {
             log::warn!("{:?}", e);
